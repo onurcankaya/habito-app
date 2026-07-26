55bb5dab77e93f005069199769ab35eda48e30ff
Onur <onur.eren.cankaya@gmail.com>
2026-07-26 12:47:56 +0200

    feat: add alert and dialog UI components

--- apps/client/src/components/ui/index.ts
+++ apps/client/src/components/ui/index.ts
@@ -1,16 +1,35 @@
+import { Alert, AlertDescription } from "./alert";
 import { Button } from "./button";
 import { Card, CardHeader, CardTitle, CardContent } from "./card";
 import { Checkbox } from "./checkbox";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter,
+  DialogClose,
+} from "./dialog";
 import { Input } from "./input";
 import { Select } from "./select";
 
 export {
+  Alert,
+  AlertDescription,
   Button,
   Card,
   CardHeader,
   CardTitle,
   CardContent,
   Checkbox,
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter,
+  DialogClose,
   Input,
   Select,
 };
